108887e9d79a093b3ff0b70e2c8704315886d83e
Kaywinnet Lee Frye <keyfrye@protonmail.com>
2019-08-24 13:39:22 +0300

    Add: bot can open / use a software wallet

--- le-bot/index.js
+++ le-bot/index.js
@@ -3,77 +3,101 @@ const express = require('express');
 const tx = require('ethereumjs-tx');
 const package = require('./package.json');
 const cron = require('node-cron');
+const { open: openWallet } = require('@colony/purser-software');
 
 const app = express();
 
+/*
+ * Optional
+ */
 const MODE_DEV = 'development';
 const NODE_ENV = process.env.NODE_ENV || MODE_DEV;
 const NETWORK = process.env.NETWORK || 'goerli';
-const INFURA_KEY = process.env.INFURA_KEY;
 const CRON_SCHEDULE = process.env.CRONTAB || '0 * * * *';
 const MANUAL_TRIGGER_URL = '/manualnoms';
+
+/*
+ * Required
+ */
+const INFURA_KEY = process.env.INFURA_KEY;
+const MNEMONIC = process.env.MNEMONIC;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 if (!INFURA_KEY) {
   console.error('Please specify the env INFURA_KEY');
   process.exit(1);
 }
 
-const web3Provider = new web3(new web3.providers.HttpProvider(`https://${NETWORK}.infura.io/${INFURA_KEY}`));
+if (!(MNEMONIC || PRIVATE_KEY)) {
+  console.error('Please specify either a env MNEMONIC or an env PRIVATE_KEY');
+  process.exit(1);
+}
 
-const sendNoms = () => {
-  // var myAddress = 'ADDRESS_THAT_SENDS_TRANSACTION';
-  // var privateKey = Buffer.from('YOUR_PRIVATE_KEY', 'hex')
-  // var toAddress = 'ADRESS_TO_SEND_TRANSACTION';
+(async () => {
+  const web3Provider = new web3(new web3.providers.HttpProvider(`https://${NETWORK}.infura.io/${INFURA_KEY}`));
 
-  // //contract abi is the array that you can get from the ethereum wallet or etherscan
-  // var contractABI = YOUR_CONTRACT_ABI;
-  // var contractAddress = "YOUR_CONTRACT_ADDRESS";
-  // //creating contract object
-  // var contract = new web3js.eth.Contract(contractABI, contractAddress);
+  const wallet = await openWallet({
+    privateKey: PRIVATE_KEY,
+    mnemonic: MNEMONIC,
+  });
 
-  // var count;
-  // // get transaction count, later will used as nonce
-  // web3js.eth.getTransactionCount(myAddress).then(function (v) {
-  //   console.log("Count: " + v);
-  //   count = v;
-  //   var amount = web3js.utils.toHex(1e16);
-  //   //creating raw tranaction
-  //   var rawTransaction = { "from": myAddress, "gasPrice": web3js.utils.toHex(20 * 1e9), "gasLimit": web3js.utils.toHex(210000), "to": contractAddress, "value": "0x0", "data": contract.methods.transfer(toAddress, amount).encodeABI(), "nonce": web3js.utils.toHex(count) }
-  //   console.log(rawTransaction);
-  //   //creating tranaction via ethereumjs-tx
-  //   var transaction = new Tx(rawTransaction);
-  //   //signing transaction with private key
-  //   transaction.sign(privateKey);
-  //   //sending transacton via web3js module
-  //   web3js.eth.sendSignedTransaction('0x' + transaction.serialize().toString('hex'))
-  //     .on('transactionHash', console.log);
+  const sendNoms = () => {
+    // var myAddress = 'ADDRESS_THAT_SENDS_TRANSACTION';
+    // var privateKey = Buffer.from('YOUR_PRIVATE_KEY', 'hex')
+    // var toAddress = 'ADRESS_TO_SEND_TRANSACTION';
 
-  //   contract.methods.balanceOf(myAddress).call()
-  //     .then(function (balance) { console.log(balance) });
-  // })
-};
+    // //contract abi is the array that you can get from the ethereum wallet or etherscan
+    // var contractABI = YOUR_CONTRACT_ABI;
+    // var contractAddress = "YOUR_CONTRACT_ADDRESS";
+    // //creating contract object
+    // var contract = new web3js.eth.Contract(contractABI, contractAddress);
 
-cron.schedule(CRON_SCHEDULE, () => {
+    // var count;
+    // // get transaction count, later will used as nonce
+    // web3js.eth.getTransactionCount(myAddress).then(function (v) {
+    //   console.log("Count: " + v);
+    //   count = v;
+    //   var amount = web3js.utils.toHex(1e16);
+    //   //creating raw tranaction
+    //   var rawTransaction = { "from": myAddress, "gasPrice": web3js.utils.toHex(20 * 1e9), "gasLimit": web3js.utils.toHex(210000), "to": contractAddress, "value": "0x0", "data": contract.methods.transfer(toAddress, amount).encodeABI(), "nonce": web3js.utils.toHex(count) }
+    //   console.log(rawTransaction);
+    //   //creating tranaction via ethereumjs-tx
+    //   var transaction = new Tx(rawTransaction);
+    //   //signing transaction with private key
+    //   transaction.sign(privateKey);
+    //   //sending transacton via web3js module
+    //   web3js.eth.sendSignedTransaction('0x' + transaction.serialize().toString('hex'))
+    //     .on('transactionHash', console.log);
 
-  console.log('Sending nom nom noms now ...');
+    //   contract.methods.balanceOf(myAddress).call()
+    //     .then(function (balance) { console.log(balance) });
+    // })
+  };
 
-  sendNoms();
+  cron.schedule(CRON_SCHEDULE, () => {
 
-});
-
-if (NODE_ENV === MODE_DEV) {
-
-  app.get(MANUAL_TRIGGER_URL, function (req, res) {
-
-    res.send('Sending nom nom noms manually...');
+    console.log('Sending nom nom noms now ...');
 
     sendNoms();
 
   });
 
-}
+  if (NODE_ENV === MODE_DEV) {
 
-app.listen(3000, () => {
-  console.log(`${package.name} started!`);
-  console.log('Listening on http://127.0.0.1:3000');
-});
+    app.get(MANUAL_TRIGGER_URL, function (req, res) {
+
+      res.send('Sending nom nom noms manually...');
+
+      sendNoms();
+
+    });
+
+  }
+
+  app.listen(3000, () => {
+    console.log(`${package.name} started!`);
+    console.log('Listening on http://127.0.0.1:3000');
+    console.log(`Using wallet ${wallet.address}`);
+  });
+
+})()
